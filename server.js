2f8db0d78fed5aaee2a2bff4f8e6c2ddb9062c52
Rahul Vig <rahvee51395@gmail.com>
2019-09-04 18:58:22 -0400

    ready for production

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express"); //common js, need to implement babel or typescript for import
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 connectDB();
 
@@ -9,14 +10,19 @@ connectDB();
 // @ts-ignore
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the ContactKeeper API..." })
-);
-
 //Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+//Serve Static Assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
